88a21c7222234f3c255d5785c96db6fd36021dcc
talhaakpinar <64715935+talhaakpinar@users.noreply.github.com>
2021-01-12 16:15:45 +0300

    Update exercise 3.py

--- exercise 3.py
+++ exercise 3.py
@@ -1,12 +1,16 @@
 class Ogrenci:
-    ogrenciAdı = "Talha"
-    ogrenciSoyadı = "Akpinar"
-    ogrenciSınıf = "11-C"
+    ogrenciAdı = input("Öğrenci Adını Giriniz: ")
+    ogrenciSoyadı = input("Öğrenci Soyadını Giriniz: ")
+    ogrenciSınıf = input("Öğrenci Sınıfını Giriniz: ")
+
+
 class Soru:
     def NetSayısı(dogru_sayisi, yanlis_sayisi):
         return dogru_sayisi - (yanlis_sayisi / 4)
     def Hesapla(net_sayisi):
         return net_sayisi * 2
+
+
 dogru_sayisi = int(input("Dogru Sayısı: "))
 yanlis_sayisi = int(input("Yanlis Sayısı: "))
 net_sayisi = Soru.NetSayısı(dogru_sayisi, yanlis_sayisi)
